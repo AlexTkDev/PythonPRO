3a8c61fd2e4498908ce487cf43d1793d8ab59b1a
alex <a8696tk@gmail.com>
2024-04-30 09:55:14 +0300

    add second version solution

--- oop_22.04.24/main.py
+++ oop_22.04.24/main.py
@@ -1,40 +1,77 @@
 import random
 
 
+# class Car:
+#     def __init__(self, model, color, trip_distance=0):
+#         self.fuel = random.randrange(0, 9)
+#         self.trip_distance = trip_distance
+#         self.model = model
+#         self.color = color
+#
+#     def move(self, distance):
+#         self.trip_distance += distance
+#         self.fuel -= distance
+#
+#     def correct_fuel(self):
+#         return max(0, self.fuel)
+#
+#
+# if __name__ == '__main__':
+#     cars = [
+#         Car("Mercedes", "White"),
+#         Car("Ford", "Blue"),
+#         Car("BMW", "Black")
+#     ]
+#
+#     desired_dist = 5
+#
+#     while True:
+#         for car in cars:
+#             car.move(random.randrange(0, 9))
+#             if car.trip_distance >= desired_dist:
+#                 print(f"{car.color} {car.model} ПЕРЕМІГ! Пройдена відстань: {car.trip_distance} км.\n")
+#                 break
+#         else:
+#             continue
+#         break
+#
+#     for i, car in enumerate(cars, 1):
+#         print(
+#             f"{i}. {car.color} {car.model} проїхав {car.trip_distance} км. Залишок палива: {car.correct_fuel()} л.")
+
+# or
+
 class Car:
-    def __init__(self, model, color, trip_distance=0):
+    def __init__(self, model: str, color: str):
         self.fuel = random.randrange(0, 9)
-        self.trip_distance = trip_distance
+        self.trip_distance = random.randrange(0, 9)
         self.model = model
         self.color = color
 
-    def move(self, distance):
-        self.trip_distance += distance
-        self.fuel -= distance
+    def move(self) -> None:
+        distance = random.randrange(0, 9)
+        move_distance = min(distance, self.fuel)
+        self.trip_distance += move_distance
+        self.fuel -= move_distance
 
-    def correct_fuel(self):
-        return max(0, self.fuel)
+    def __repr__(self) -> str:
+        return f"<{self.model}: fuel={self.fuel} distance={self.trip_distance}>"
 
 
-if __name__ == '__main__':
-    cars = [
-        Car("Mercedes", "White"),
-        Car("Ford", "Blue"),
-        Car("BMW", "Black")
-    ]
+MODEL_COLOR = [("DODGE", "RED"), ("CHEVROLET", "YELLOW"), ("FORD", "ORANGE")]
+cars = [Car(model=model, color=color) for model, color in MODEL_COLOR]
 
-    desired_dist = 5
+desired_dist = random.randrange(3, 9)
 
-    while True:
-        for car in cars:
-            car.move(random.randrange(0, 9))
+winner = None
+while not winner:
+    for car in cars:
+        if car.fuel > 0:
+            car.move()
             if car.trip_distance >= desired_dist:
-                print(f"{car.color} {car.model} ПЕРЕМІГ! Пройдена відстань: {car.trip_distance} км.\n")
+                winner = car
+                print(f"Winner is {winner.model} with distance {winner.trip_distance}")
                 break
-        else:
-            continue
-        break
 
-    for i, car in enumerate(cars, 1):
-        print(
-            f"{i}. {car.color} {car.model} проїхав {car.trip_distance} км. Залишок палива: {car.correct_fuel()} л.")
+for car in cars:
+    print(car)
