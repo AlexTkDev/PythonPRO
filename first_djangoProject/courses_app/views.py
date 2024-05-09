a60b384ddc5163944bf2f756c656b38a1eae6265
alex <a8696tk@gmail.com>
2024-05-09 19:14:13 +0300

    fix output

--- first_djangoProject/courses_app/views.py
+++ first_djangoProject/courses_app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 
 def is_auntification(request):
-    context = {"message": f"Авторизованный пользователь: {request.user.username}"}
+    context = {"message": f"This is the Courses page: user - {request.user.username}"}
     if request.user.is_anonymous:
-        context.update({"message": "Анонимный пользователь, авторизуйтесь!"})
+        context.update({"message": "You have no permissions to view this page!"})
         return render(request, "auntification_information.html", context)
     return render(request, "auntification_information.html", context)
 
