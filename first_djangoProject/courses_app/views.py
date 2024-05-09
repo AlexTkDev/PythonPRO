83fc586137039fae4704454ff7594a5f1c34c35f
alex <a8696tk@gmail.com>
2024-05-09 11:16:26 +0300

    fix performed check authorization

--- first_djangoProject/courses_app/views.py
+++ first_djangoProject/courses_app/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def is_auntification(request):
-    message = {"message": f"Авторизованный пользователь: {request.user.username}"}
+    context = {"message": f"Авторизованный пользователь: {request.user.username}"}
     if request.user.is_anonymous:
-        message.update({"message": "Анонимный пользователь, авторизуйтесь!"})
-        return render(request, "auntification_information.html", message)
-    return render(request, "auntification_information.html", message)
+        context.update({"message": "Анонимный пользователь, авторизуйтесь!"})
+        return render(request, "auntification_information.html", context)
+    return render(request, "auntification_information.html", context)
+
