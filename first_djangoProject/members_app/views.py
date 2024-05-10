d24bf4fe7cad8e11dc2543398ad9a54e3f05130f
alex <a8696tk@gmail.com>
2024-05-10 13:41:53 +0300

    Fix

--- first_djangoProject/members_app/views.py
+++ first_djangoProject/members_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseBadRequest
 
 
 def index(request):
@@ -8,15 +9,21 @@ def index(request):
 def input_information(request):
     if request.method == "POST":
         user_input = request.POST.get('user_input')
-        request.session['user_input'] = user_input
-        return redirect("show_information")
+        if user_input:
+            request.session['user_input'] = user_input
+            return redirect("show_information")
+        else:
+            return HttpResponseBadRequest("Invalid input!")
     else:
         return render(request, "input.html")
 
 
 def show_information(request):
-    context = {"user_input": request.session.get("user_input")}
-    return render(request, "show.html", context)
+    if request.method == "GET":
+        context = {"user_input": request.session.get("user_input")}
+        return render(request, "show.html", context)
+    else:
+        return HttpResponseBadRequest("Invalid request method")
 
 
 
