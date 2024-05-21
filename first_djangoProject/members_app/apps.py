27d28895d61dad65cd49eb183329fe0b87b6195e
alex <a8696tk@gmail.com>
2024-05-21 14:40:12 +0300

    added name to admin site

--- first_djangoProject/members_app/apps.py
+++ first_djangoProject/members_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MembersAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "members_app"
+    verbose_name = "Сохранённый ввод"
