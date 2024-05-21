9d9b4cc7b557d1498b32389365cdf3c9bd99518d
alex <a8696tk@gmail.com>
2024-05-21 14:41:28 +0300

    added class MemberAdmin to "members_app/admin.py"

--- first_djangoProject/members_app/admin.py
+++ first_djangoProject/members_app/admin.py
@@ -4,4 +4,9 @@ from members_app.models import Member
 # Register your models here.
 
 
-admin.site.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('id', "save_text")
+    search_fields = ('id', "save_text")
+
+
+admin.site.register(Member, MemberAdmin)
