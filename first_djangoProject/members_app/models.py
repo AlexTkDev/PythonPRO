8981df75d14b451a68b24a0cb56a5b03cec585ef
alex <a8696tk@gmail.com>
2024-05-21 14:42:25 +0300

    added  class Meta to customizations "members_app/models.py"

--- first_djangoProject/members_app/models.py
+++ first_djangoProject/members_app/models.py
@@ -4,5 +4,9 @@ from django.db import models
 class Member(models.Model):
     save_text = models.TextField()
 
+    class Meta:
+        verbose_name = 'введённый текст'
+        verbose_name_plural = "всё что мы ввели:"
+
     def __str__(self):
         return self.save_text
