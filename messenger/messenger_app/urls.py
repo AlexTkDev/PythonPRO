f0f78cf0c0d60824ae67eb611ae5be40127d9fe7
alex <a8696tk@gmail.com>
2024-05-22 16:00:24 +0300

    realization function show messages on general page

--- messenger/messenger_app/urls.py
+++ messenger/messenger_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, save_text
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('save_text/', save_text, name='save_text'),
 ]
--- messenger/messenger_app/views.py
+++ messenger/messenger_app/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from .models import UserMessage
 
 
 # Create your views here.
-
 def index(request):
-    return render(request, "messenger_app/base.html")
+    messages = UserMessage.objects.all().order_by('-date_sent')
+    context = {
+        "messages": messages,
+        "title": "Messenger",
+    }
+    return render(request, "messenger_app/base.html", context)
+
+
+@login_required
+def save_text(request):
+    if request.method == "POST":
+        message_text = request.POST.get("message_text", "")
+        if message_text:
+            UserMessage.objects.create(message=message_text, author=request.user)
+            return redirect('index')
+        else:
+            messages = UserMessage.objects.all().order_by('-date_sent')
+            context = {
+                "messages": messages,
+                "error": "Сообщение не может быть пустым.",
+                "title": "Messenger"
+            }
+            return render(request, "messenger_app/base.html", context)
+    return redirect('index')
